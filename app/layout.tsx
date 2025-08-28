18696abec5cd3335f345eca2a659bc7d54013841
nikatopu <nikatopu@gmail.com>
2025-08-28 23:33:48 +0400

    Fix/ updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,70 @@ import "./global.scss";
 import { AppProvider } from "@/lib/AppContext"; // Import the provider
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.geowonder.tours"),
   title: {
-    template: "%s | GeoWonder", // Page titles will be "Page Name | GeoWonder"
-    default: "GeoWonder - Tours in Georgia", // Default title for the homepage
+    template: "%s | GeoWonder",
+    default: "GeoWonder - Unforgettable Tours in Georgia",
   },
   description:
-    "Discover the beauty of Georgia with GeoWonder. We offer unique tours, breathtaking landscapes, and unforgettable experiences.",
-  // More metadata can be added here (e.g., openGraph, twitter)
+    "Discover the beauty of Georgia with GeoWonder. We offer unique tours, breathtaking landscapes, and authentic cultural experiences.",
+  keywords: [
+    "Georgia tours",
+    "travel Georgia",
+    "Caucasus mountains",
+    "Tbilisi",
+    "Batumi",
+    "Svaneti",
+    "Kazbegi",
+    "Georgian wine tour",
+    "Georgian cuisine",
+    "Georgian culture",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+
+  icons: {
+    icon: "/geowonder-full.jpg",
+  },
+
+  manifest: "/site.webmanifest",
+
+  openGraph: {
+    title: "GeoWonder - Unforgettable Tours in Georgia",
+    description:
+      "Discover the beauty of Georgia with our unique tours, breathtaking landscapes, and authentic cultural experiences.",
+    url: "https://www.geowonder.tours",
+    siteName: "GeoWonder",
+    images: [
+      {
+        url: "/geo-wonder.jpg",
+        width: 1024,
+        height: 1024,
+        alt: "A stunning collage of Georgian landscapes and culture.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "GeoWonder - Unforgettable Tours in Georgia",
+    description:
+      "Discover the beauty of Georgia with our unique tours, breathtaking landscapes, and authentic cultural experiences.",
+    images: ["/geo-wonder.jpg"],
+  },
+
+  themeColor: "#6B7D47",
 };
 
 export default function RootLayout({
